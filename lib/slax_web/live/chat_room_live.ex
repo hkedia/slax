b40b1f1292a0b76704de9d71cb70156dbb548520
Harsh Kedia <hkedia@hey.com>
2024-06-19 02:44:03 +0530

    use patch instead of navigate, move some code from mount to handle_params

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -52,7 +52,7 @@ defmodule SlaxWeb.ChatRoomLive do
         "flex items-center h-8 text-sm pl-8 pr-3",
         (@active && "bg-slate-300") || "hover:bg-slate-300"
       ]}
-      navigate={~p"/rooms/#{@room}"}
+      patch={~p"/rooms/#{@room}"}
     >
       <.icon name="hero-hashtag" class="h-4 w-4" />
       <span class={["ml-2 leading-none", @active && "font-bold"]}>
@@ -62,25 +62,32 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
 
-  def mount(params, _session, socket) do
+  def mount(_params, _session, socket) do
     rooms = Repo.all(Room)
 
+    socket =
+      socket
+      |> assign(rooms: rooms)
+
+    {:ok, socket}
+  end
+
+  def handle_params(params, _uri, socket) do
     room =
       case Map.fetch(params, "id") do
         {:ok, id} ->
           Repo.get!(Room, id)
 
         :error ->
-          List.first(rooms)
+          List.first(socket.assigns.rooms)
       end
 
     socket =
       socket
-      |> assign(room: room)
       |> assign(hide_topic?: false)
-      |> assign(rooms: rooms)
+      |> assign(room: room)
 
-    {:ok, socket}
+    {:noreply, socket}
   end
 
   def handle_event("toggle-topic", _, socket) do
