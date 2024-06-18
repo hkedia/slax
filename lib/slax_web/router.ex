feb882a672a188ab3c12795d00796e27cecc89ab
Harsh Kedia <hkedia@hey.com>
2024-06-19 02:30:15 +0530

    add href to links

--- lib/slax_web/router.ex
+++ lib/slax_web/router.ex
@@ -18,6 +18,8 @@ defmodule SlaxWeb.Router do
     pipe_through :browser
 
     live "/", ChatRoomLive
+    live "/rooms", ChatRoomLive
+    live "/rooms/:id", ChatRoomLive
   end
 
   # Other scopes may use custom stacks.
